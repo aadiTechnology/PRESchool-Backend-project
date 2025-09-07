ab7d6042becf1072eea645ade3bdc308ee0a5bc8
ajitsagale <ajitsagale@gmail.com>
2025-09-07 19:51:23 +0530

    configuration anSENDGRID_FROM_EMAILd start

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,16 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str = ""
+    SQLALCHEMY_DATABASE_URL: str = "mssql+pyodbc://LAPTOP-FC61VAQA\\SQLEXPRESS/erpdb?driver=ODBC+Driver+18+for+SQL+Server&Trusted_Connection=yes&TrustServerCertificate=yes"
     SECRET_KEY: str = ""
     ALGORITHM: str = ""
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     SENDGRID_FROM_EMAIL: str = ""
     SENDGRID_API_KEY: str = ""
-
+    HOMEWORK_UPLOAD_DIR: str = "uploads/homeworks"  # <-- Add this
+    API_URL: str = "http://localhost:8000"  # <-- Add this
+    
     class Config:
         env_file = ".env"
 
